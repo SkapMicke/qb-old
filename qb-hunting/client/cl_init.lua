0eea0617fcd12d837c4ab8be6ca367deae7325c1
SkapMicke <161764761+SkapMicke@users.noreply.github.com>
2024-08-25 10:00:33 +0200

    removed.

--- qb-hunting/client/cl_init.lua
+++
@@ -1,6 +0,0 @@
---Citizen.CreateThread(function()
---    exports["PolyZone"]:AddCircleZone("huntingsales", vector3(-773.28, 5596.95, 33.49), 2.0, {
---        useZ = true,
---        debugPoly= true,
---    })
---end)
